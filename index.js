4db2991ee2fcb49c08748f6547903514cf0bfc76
Devolpment <devolpment@MacBook-Pro-3.local>
2024-07-23 16:02:58 -0700

    added render function, css code to make two sections

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const playerContainer = document.querySelector(".selecting-players-container");
+let playersListArray = [];
 async function fetchPlayer(url) {
   let playersList;
   try {
@@ -12,24 +13,35 @@ async function fetchPlayer(url) {
   return playersList;
 }
 
-let playersListArray = fetchPlayer(
-  "./6th Division North Vancouver III - British Columbia Mainland Cricket League.json"
-);
-
-playersListArray.then((player) => {
-  player.forEach((item) => {
+function renderPlayerList(list) {
+  list.forEach((item) => {
     const playerName = document.createElement("div");
     const playerId = document.createElement("div");
+    const playerAction = document.createElement("div");
     const playerCard = document.createElement("div");
 
     playerName.classList.add("player-name");
     playerId.classList.add("player-id");
+    playerAction.classList.add("player-action");
+    playerAction.classList.add("selecting");
     playerCard.classList.add("player-card");
 
     playerName.textContent = item["Player Name"];
     playerId.textContent = item["CC Player Id"];
+    playerAction.textContent = "a";
 
-    playerCard.append(playerName, playerId);
+    playerCard.append(playerName, playerId, playerAction);
     playerContainer.append(playerCard);
   });
+}
+
+//fetch the list of the players and spread the array into playerListArray
+fetchPlayer(
+  "./6th Division North Vancouver III - British Columbia Mainland Cricket League.json"
+).then((player) => {
+  playersListArray = [...player];
+  renderPlayerList(playersListArray);
 });
+
+// event listener to select the players from the selecting list
+//playerContainer.addEventListener("click", selectPlayer);
